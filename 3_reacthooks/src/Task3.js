75bb09b5692404fe6ad3217206353438592bf5b1
marininru <alexey@marinin.ru>
2023-03-14 09:11:26 +0300

    Переименовать каталог react-приложения

--- 3_reacthooks/src/Task3.js
+++
@@ -1,7 +0,0 @@
-import { useHover } from './hooks/useHover';
-
-export function Task3() {
-    const { hovered, ref } = useHover();
-
-    return <div ref={ref}>{hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}</div>;
-}
